0c3c9c9eca64f4d1146bd4b871366265aba154a9
Shardul Chaudhary <shardul@99homes.co.uk>
2025-10-22 09:19:57 +0530

    updated backend

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use(cors({
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
     'https://commercial-uk.vercel.app',
-    'https://www.commercialuk.co.uk/'
+    'https://www.commercialuk.co.uk'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
