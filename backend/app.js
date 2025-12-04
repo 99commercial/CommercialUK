e2c0e4b69b8ca0344642b3306333bfa0ca32da7c
Shardul Chaudhary <shardul1210@gmail.com>
2025-12-04 17:24:04 +0530

    Fix CORS for VPS

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,8 @@ app.use(cors({
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
     'https://commercial-uk.vercel.app',
-    'https://www.commercialuk.co.uk'
+    'https://www.commercialuk.co.uk',
+    'http://72.61.202.155',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
